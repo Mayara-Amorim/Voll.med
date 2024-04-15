84a942d7cdc98431b5f8ddd789f567fee2cb80e5
Mayara Amorim <mayara.moreira@fenixeditorial.com.br>
2024-04-15 20:49:50 -0300

    FEAT(segurança): Implementando especificacao Servelet: Filter e verificaÃo de token no cabeçalho

---
+++ src/main/java/med/voll/api/infra/security/SecurityFilter.java
@@ -0,0 +1,27 @@
+package med.voll.api.infra.security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.lang.model.type.NullType;
+import java.io.IOException;
+
+public class SecurityFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        var tokenJWT = recuperarToken(request);
+
+        filterChain.doFilter(request, response);
+    }
+
+    private String recuperarToken(HttpServletRequest request) {
+        var authorizationHeader = request.getHeader("Authorization");
+        if(authorizationHeader == null) {
+            throw new RuntimeException("Token não enviado no cabeçalho");
+        }
+        return authorizationHeader.replace("Bearer", "");
+    }
+}
